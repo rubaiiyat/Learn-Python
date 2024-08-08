4439b4004334f1465b35b2db5af1cdf93b7c5c60
rubaiiyat <abirr1516@gmail.com>
2024-08-08 18:56:41 +0600

    escape sequence

--- 1-Primitive-Types/02-Escape_sequences.py
+++ 1-Primitive-Types/02-Escape_sequences.py
@@ -10,9 +10,12 @@ learn2='Python \"Programming'
 learn3='Python\nProgramming'
 learn4='Python\\ Programming'
 learn5='Python\t Programming'
+learn6='Python\f Programming'
+
 
 print(learn)
 print(learn2)
 print(learn3)
 print(learn4)
 print(learn5)
+print(learn6)
