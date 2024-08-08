bf11004bdba36ca9a7d5287170852d7fe6eb6210
rubaiiyat <abirr1516@gmail.com>
2024-08-08 18:44:25 +0600

    first commit

---
+++ 1-Primitive-Types/01-string.py
@@ -0,0 +1,18 @@
+learn="Python Programming"
+
+print(len(learn))
+print(learn[:5])
+print(learn[5:10])
+print(learn[5:])
+print(learn[-5:])
+print(learn[:-5])
+print(learn[-5:-2])
+
+
+message="""
+Hello John,
+I hope you're well.
+Thank you
+"""
+
+print(message)
