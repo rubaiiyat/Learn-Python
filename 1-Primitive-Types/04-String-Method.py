d3839cdaacccc98228f3a2cbb0c60086260dc059
rubaiiyat <abirr1516@gmail.com>
2024-08-08 23:20:43 +0600

    string method

---
+++ 1-Primitive-Types/04-String-Method.py
@@ -0,0 +1,12 @@
+learn=' Python Programming '
+
+print(learn.upper())
+print(learn.lower())
+print(learn.strip())
+print(learn.lstrip())
+print(learn.rstrip())
+print(learn.find('mming'))
+print(learn.replace('Python',"C++"))
+print("Python" in learn)
+print("C++" in learn)
+print("C++" not in learn)
