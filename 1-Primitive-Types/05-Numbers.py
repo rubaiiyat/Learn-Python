399bcf0d89bebb639f90848ebfbc03a9deb4ee40
rubaiiyat <abirr1516@gmail.com>
2024-08-09 18:37:25 +0600

    numbers

---
+++ 1-Primitive-Types/05-Numbers.py
@@ -0,0 +1,18 @@
+import math
+
+print(round(5.6))
+print(abs(-2.6))
+print(math.ceil(2.4))
+print(math.floor(2.8))
+
+
+a=5
+b=6
+
+print(math.pow(a,b))
+print(math.pi*a*b)
+print(math.asin(0.55))
+print(math.sqrt(a))
+print(math.factorial(b))
+print(math.lcm(a,b))
+print(math.gcd(a,b))
