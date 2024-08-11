304924b1a5213087281c3055335d40d26bbc1ed5
rubaiiyat <abirr1516@gmail.com>
2024-08-11 15:09:14 +0600

    for else

---
+++ 2-Control-Flow/06-for-else.py
@@ -0,0 +1,11 @@
+successfull=False
+
+for i in range(3):
+    print('Attempt')
+    if successfull:
+        print('Success')
+        break
+
+else:
+    print('Your 3 times attempt are failed')
+        
