d94c67dc4e64a864df0d56621e230771c89d212f
rubaiiyat <abirr1516@gmail.com>
2024-08-15 17:12:21 +0600

    xxargs

---
+++ 3- Functions/06-xxargs.py
@@ -0,0 +1,7 @@
+def student_data(**data):
+    print(data["name"])
+
+
+student_data(name="John", id=4543, address="Munich")
+student_data(name="Don", id=3143, address="Berlin")
+student_data(name="Mong", id=9000, address="Hamburg")
