582fcd8340185867936a837147354c21941161bf
rubaiiyat <abirr1516@gmail.com>
2024-08-19 19:14:43 +0600

    list comprehensions

--- 4-Data-Structures/11-list-comprehensions.py
+++ 4-Data-Structures/11-list-comprehensions.py
@@ -1,7 +1,7 @@
 items = [
     {"name": "Rice", "price": 60, "quantity": 5},
     {"name": "Sugar", "price": 70, "quantity": 3},
-    {"name": "Lentils", "price": 110, "quantity": 2},
+    {"name": "Lentils", "price": 100, "quantity": 2},
     {"name": "Cooking Oil", "price": 150, "quantity": 2},
     {"name": "Flour", "price": 50, "quantity": 5},
     {"name": "Salt", "price": 30, "quantity": 1},
@@ -11,6 +11,9 @@ items = [
     {"name": "Tea", "price": 120, "quantity": 0.5},
 ]
 
-prices = list(filter(lambda item: item["price"] > 100, items))
-
+prices = [item["price"] for item in items]
 print(prices)
+
+filtered = [item["price"] for item in items if 100 >= item["price"] >= 50]
+
+print(filtered)
