2069f27374e6c6e8fd6026de9f30f45e886bfc5b
rubaiiyat <abirr1516@gmail.com>
2024-08-19 18:50:39 +0600

    map

---
+++ 4-Data-Structures/09-map.py
@@ -0,0 +1,20 @@
+items = [
+    {"name": "Rice", "price": 60, "quantity": 5},
+    {"name": "Sugar", "price": 70, "quantity": 3},
+    {"name": "Lentils", "price": 110, "quantity": 2},
+    {"name": "Cooking Oil", "price": 150, "quantity": 2},
+    {"name": "Flour", "price": 50, "quantity": 5},
+    {"name": "Salt", "price": 30, "quantity": 1},
+    {"name": "Soap", "price": 25, "quantity": 5},
+    {"name": "Shampoo", "price": 200, "quantity": 1},
+    {"name": "Toothpaste", "price": 90, "quantity": 1},
+    {"name": "Tea", "price": 120, "quantity": 0.5},
+]
+
+
+""" for item in items:
+    prices.append(item["name"]) """
+
+prices = list(map(lambda item: item["price"], items))
+
+print(prices)
