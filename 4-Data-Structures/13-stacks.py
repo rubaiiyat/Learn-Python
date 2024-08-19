fa6f4ec52a192838a9ec5c5c3ce39396a7dbf5f4
rubaiiyat <abirr1516@gmail.com>
2024-08-20 00:00:03 +0600

    stack

---
+++ 4-Data-Structures/13-stacks.py
@@ -0,0 +1,15 @@
+browser_Session = []
+browser_Session.append(1)
+browser_Session.append(2)
+browser_Session.append(3)
+browser_Session.append(4)
+
+print(browser_Session)
+
+
+browser_Session.pop()
+print(browser_Session)
+
+
+browser_Session.pop()
+print(browser_Session)
