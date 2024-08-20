1ed60c6fb623d172da35c38dd370915be9e9b701
rubaiiyat <abirr1516@gmail.com>
2024-08-20 19:19:38 +0600

    dictionary

---
+++ 4-Data-Structures/18-dictionary.py
@@ -0,0 +1,13 @@
+item = {"Name": "potato", "Price:": "20"}
+item["quantity"] = "1kg"
+item["from"] = "japan"
+
+print(item)
+
+
+mydict = {x: x * 2 for x in range(5)}
+
+print(mydict)
+
+mytuple = tuple((y * 2 for y in range(5)))
+print(mytuple)
