0af8628589fc11336de1cf177e80acda3abd8b69
rubaiiyat <abirr1516@gmail.com>
2024-08-22 23:56:10 +0600

    handling different exception

---
+++ 05-Exceptions/02-Handling-different-exceptions.py
@@ -0,0 +1,9 @@
+try:
+    age = int(input())
+    val = int(input())
+    result = age / val
+    print("Successful")
+except ValueError:
+    print("Please Enter the valid input")
+except ZeroDivisionError:
+    print("Division by zero is not possible")
