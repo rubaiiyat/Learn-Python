327df9fc0b47393525f38775b58211f18e524eb1
rubaiiyat <abirr1516@gmail.com>
2024-08-22 23:47:28 +0600

    handling exceptions

---
+++ 05-Exceptions/01-Handling-exceptions.py
@@ -0,0 +1,8 @@
+try:
+    age = int(input())
+    if age >= 18:
+        print("You are eligible to create an account")
+    else:
+        print("You are not eligible to create an account")
+except ValueError:
+    print("Please enter the valid number")
