ef12f1a4b91129553dce06dcc421659f5a77148a
rubaiiyat <abirr1516@gmail.com>
2024-08-23 23:35:16 +0600

    rasing exceptions

---
+++ 05-Exceptions/03-Cleaning-up.py
@@ -0,0 +1,14 @@
+try:
+
+    file = open("app.py")
+    age = int(input())
+    val = int(input())
+    result = age / val
+    print("Successful")
+except ValueError:
+    print("Please Enter the valid input")
+except ZeroDivisionError:
+    print("Division by zero is not possible")
+
+finally:
+    file.close()
---
+++ 05-Exceptions/04-Rasing-exceptions.py
@@ -0,0 +1,10 @@
+def calculations(age):
+    if age <= 0:
+        raise ValueError("You can not divided by 0")
+    return 50 / age
+
+
+try:
+    print(calculations(-1))
+except ValueError as value:
+    print(value)
