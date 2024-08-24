59f2e90fdcffc4ba13f91bac57c089da9374f67b
rubaiiyat <abirr1516@gmail.com>
2024-08-25 01:39:50 +0600

    Magic method

---
+++ 06-OOP/05-Magic-method.py
@@ -0,0 +1,17 @@
+class Animal:
+    def __init__(
+        self,
+        name,
+        food,
+    ) -> None:
+        self.name = name
+        self.food = food
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}, Food: {self.food}"
+
+
+print(Animal("Lion", "Meat"))
+print(Animal("Elephant", "Grass"))
+print(Animal("Panda", "Bamboo"))
+print(Animal("Rabbit", "Carrots"))
