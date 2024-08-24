5c5e080ff07d59becd7da815cdf109d6dcf790de
rubaiiyat <abirr1516@gmail.com>
2024-08-24 23:49:17 +0600

    Class

---
+++ 06-OOP/01-Class.py
@@ -0,0 +1,22 @@
+class Student:
+    name = ""
+    roll = ""
+    cls = ""
+    gpa = ""
+
+
+abir = Student()
+abir.name = "Abir"
+abir.roll = 43
+abir.cls = "CSE17"
+abir.gpa = "3.00"
+print(f"(Name: {abir.name}, Roll: {abir.roll}, Class: {abir.cls}, GPA: {abir.gpa})")
+
+
+Rubaiyat = Student()
+Rubaiyat.name = "Rubaiyat"
+Rubaiyat.roll = 44
+Rubaiyat.gpa = "3.00"
+print(
+    f"(Name: {Rubaiyat.name}, Roll: {Rubaiyat.roll}, Class: {Rubaiyat.cls}, GPA: {Rubaiyat.gpa})"
+)
