623bff28b9a51d9ae957b406f766ef0c513c6f28
rubaiiyat <abirr1516@gmail.com>
2024-08-25 00:30:02 +0600

    Constructor

---
+++ 06-OOP/03-Constructor.py
@@ -0,0 +1,18 @@
+class Student:
+    name = ""
+    roll = ""
+    gpa = ""
+
+    def __init__(self, name, roll, gpa) -> None:
+        self.name = name
+        self.roll = roll
+        self.gpa = gpa
+
+    def displayValue(self):
+        print(f"Name: {self.name}, Roll: {self.roll} GPA: {self.gpa}")
+
+
+abir = Student("Abir", 43, 3.0)
+abir.displayValue()
+rubaiyat = Student("Rubaiyat", 44, 3.0)
+rubaiyat.displayValue()
