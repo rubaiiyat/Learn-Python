81b48a2cdeb7a61eda749a5ae19a2ec6dbe4309b
rubaiiyat <abirr1516@gmail.com>
2024-08-25 00:02:25 +0600

    method

---
+++ 06-OOP/02-method.py
@@ -0,0 +1,25 @@
+class Student:
+    name = ""
+    roll = ""
+    cls = ""
+    gpa = ""
+
+    def setValue(self, name, roll, cls, gpa):
+        self.name = name
+        self.roll = roll
+        self.cls = cls
+        self.gpa = gpa
+
+    def displayValue(self):
+        print(
+            f"(Name: {self.name}, Roll: {self.roll}, Class: {self.cls}, GPA: {self.gpa})"
+        )
+
+
+abir = Student()
+abir.setValue("Rubaiyat", 44, "CSE17", 3.00)
+abir.displayValue()
+
+Rubaiyat = Student()
+Rubaiyat.setValue("Rubaiyat", 44, "CSE17", 3.00)
+Rubaiyat.displayValue()
