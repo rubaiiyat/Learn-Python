0fd4d7345dbd16499023456704f56a3be2c35b14
rubaiiyat <abirr1516@gmail.com>
2024-08-27 02:12:11 +0600

    Method Overriding

---
+++ 6-OOP/09-Method-overriding.py
@@ -0,0 +1,11 @@
+class phone:
+    def __init__(self) -> None:
+        print("I'm phone class")
+
+
+class iphone(phone):
+    def __init__(self) -> None:
+        print("I'm iphone class")
+
+
+a = iphone()
