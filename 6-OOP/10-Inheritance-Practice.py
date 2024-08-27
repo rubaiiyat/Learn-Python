8a0da2ea6e2f6c47a4620ff7564b09dd414802d4
rubaiiyat <abirr1516@gmail.com>
2024-08-27 18:06:44 +0600

    Inheritance Practice problem

--- 6-OOP/10-Inheritance-Practice.py
+++ 6-OOP/10-Inheritance-Practice.py
@@ -1,21 +1,22 @@
 class Shape:
-
-    def __init__(self, w, l) -> None:
-        self.w = w
-        self.l = l
+    def __init__(self, num1, num2) -> None:
+        self.num1 = num1
+        self.num2 = num2
 
 
 class Triangle(Shape):
-    def __init__(self) -> None:
-        area = 0.5 * self.w * self.l
-        print("Area:", area)
+    def __init__(self, num1, num2) -> None:
+        super().__init__(num1, num2)
+        area = 0.5 * num1 * num2
+        print("Triangle Area:", area)
 
 
 class Rectangle(Shape):
-    def __init__(self) -> None:
-        area = self.w * self.l
-        print("Area:", area)
+    def __init__(self, num1, num2) -> None:
+        super().__init__(num1, num2)
+        area = num1 * num2
+        print("Rectangle Area:", area)
 
 
-a = Triangle(10, 20)
-b = Rectangle(10, 20)
+area = Triangle(10, 20)
+area = Rectangle(10, 20)
